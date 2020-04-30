f144082bb0394c272927a9403fcb4321d0cbb650
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-30 15:04:59 +0300

    Refactoring using a list view builder

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -22,21 +22,14 @@ class _TasksListState extends State<TasksList> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTile(
-          title: tasks[0].name,
-          isChecked: tasks[0].isDone,
-        ),
-        TaskTile(
-          title: tasks[1].name,
-          isChecked: tasks[1].isDone,
-        ),
-        TaskTile(
-          title: tasks[2].name,
-          isChecked: tasks[2].isDone,
-        ),
-      ],
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (context, index) {
+        return TaskTile(
+          title: tasks[index].name,
+          isChecked: tasks[index].isDone,
+        );
+      },
     );
   }
 }
