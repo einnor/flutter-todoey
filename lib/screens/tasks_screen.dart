4d1fc4559e867a3bd486f8cee216ed692319c1ad
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-30 15:45:06 +0300

    Remove the bottom sheet after adding a task

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -45,6 +45,7 @@ class _TasksScreenState extends State<TasksScreen> {
                         ),
                       );
                     });
+                    Navigator.pop(context);
                   },
                 ),
               ),
