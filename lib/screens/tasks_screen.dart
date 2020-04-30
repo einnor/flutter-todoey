6ac4cb490dccc2fcd33b590273c13491a4873cd8
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-30 11:56:53 +0300

    Scaffold background color

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: ,
+    );
   }
 }
