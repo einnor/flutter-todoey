d1fca3aed241f1892f66da05d30ceb2b8856d553
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-30 17:59:19 +0300

    TaskData class

---
+++ lib/models/task_data.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'task.dart';
+
+class TaskData with ChangeNotifier {
+  List<Task> tasks = [
+    Task(
+      name: 'Buy milk',
+    ),
+    Task(
+      name: 'Buy eggs',
+    ),
+    Task(
+      name: 'Buy bread',
+    ),
+  ];
+}
