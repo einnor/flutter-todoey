ca314273ef0f3fa8f0dc8a810bb4df9f3cf0aa30
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-30 18:27:08 +0300

    Refactor using consumer

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -6,16 +6,20 @@ import 'task_tile.dart';
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: Provider.of<TaskData>(context).tasks.length,
-      itemBuilder: (context, index) {
-        return TaskTile(
-          title: Provider.of<TaskData>(context).tasks[index].name,
-          isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
-          onChanged: (value) {
+    return Consumer<TaskData>(
+      builder: (context, taskData, _) {
+        return ListView.builder(
+          itemCount: taskData.tasks.length,
+          itemBuilder: (context, index) {
+            return TaskTile(
+              title: taskData.tasks[index].name,
+              isChecked: taskData.tasks[index].isDone,
+              onChanged: (value) {
 //            setState(() {
 //              widget.tasks[index].toggleDone();
 //            });
+              },
+            );
           },
         );
       },
